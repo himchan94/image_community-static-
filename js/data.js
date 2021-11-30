5b45bb2afbb755284d36c432832dbeebfea671c6
himchan <himchan9494@gmail.com>
2021-11-30 17:20:07 +0900

    feat: Added global variable

--- js/data.js
+++ js/data.js
@@ -1,4 +1,7 @@
 var my_info;
+var photos = [];
+
+// global unction
 var init;
 var setMenu;
 var setFilter;
@@ -8,19 +11,8 @@ var uploadFile;
 var setDescLength;
 var setSort;
 
-var filterName = "all";
-var getFilterParams = {
-  all: function () {
-    return ["idx", ">", 0];
-  },
-  mine: function () {
-    return ["user_id", "==", my_info.id];
-  },
-  like: function () {
-    return ["idx", "in", my_info.like];
-  },
-  follow: function () {
-    return ["user_id", "in", my_info.follow];
-  },
-};
-var photos = [];
+var filterName;
+var getFilterParams;
+
+var uploadComment;
+var selectedPhoto;
